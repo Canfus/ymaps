70c8cff510c2b27b63b7fb364ebf645a0f51cfe0
Timur Mukhametyanov <canfus60@gmail.com>
2023-12-09 19:29:12 +0300

    fix prettier

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,50 +1,55 @@
-import {useEffect, useRef, useState, memo} from "react";
+import { useEffect, useRef, useState } from 'react';
 
 import type { LngLat } from '../../types/ymaps.ts';
 
 interface MapTestProps extends React.HTMLAttributes<HTMLDivElement> {
-    routePoints?: LngLat[];
-    center?: number[];
-    zoom?: number;
+  routePoints?: LngLat[];
+  center?: number[];
+  zoom?: number;
 }
 
-const Map = ({ routePoints, center = [54.81181346408782,56.0817958701094], zoom = 16, className, ...props }: MapTestProps) => {
-    const mapRef = useRef<HTMLDivElement>(null);
-    const [map, setMap] = useState<ymaps.Map | undefined>();
+const Map = ({
+  routePoints,
+  center = [54.81181346408782, 56.0817958701094],
+  zoom = 16,
+  className,
+  ...props
+}: MapTestProps) => {
+  const mapRef = useRef<HTMLDivElement>(null);
+  const [map, setMap] = useState<ymaps.Map | undefined>();
 
-    const { Map, multiRouter } = window?.ymaps;
+  const { Map, multiRouter } = window.ymaps;
 
-    const initMap = () => {
-        if (!map) {
-            const map = new Map(mapRef.current!, { center, zoom, controls: [] });
-            setMap(map);
-        }
+  useEffect(() => {
+    window.ymaps.ready(() => {
+      const map = new Map(mapRef.current!, { center, zoom, controls: [] });
+      setMap(map);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (map) {
+      map.geoObjects.removeAll();
+
+      if (routePoints) {
+        const route = new multiRouter.MultiRoute(
+          {
+            referencePoints: routePoints,
+            params: {
+              routingMode: 'auto',
+            },
+          },
+          {
+            boundsAutoApply: true,
+          },
+        );
+
+        map.geoObjects.add(route);
+      }
     }
+  }, [map, multiRouter.MultiRoute, routePoints]);
 
-    useEffect(() => {
-        window.ymaps.ready(initMap);
-    }, []);
-
-    useEffect(() => {
-        if (map) {
-            map.geoObjects.removeAll();
-
-            if (routePoints) {
-                const route = new multiRouter.MultiRoute({
-                    referencePoints: routePoints,
-                    params: {
-                        routingMode: "auto"
-                    },
-                }, {
-                    boundsAutoApply: true
-                });
-
-                map.geoObjects.add(route);
-            }
-        }
-    }, [routePoints]);
-
-    return <div ref={mapRef} className={className} {...props} />
+  return <div ref={mapRef} className={className} {...props} />;
 };
 
-export default memo(Map);
+export default Map;
